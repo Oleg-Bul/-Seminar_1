84e0130df52f32d054aaf63685d123d52388f83a
Oleg_bul <kokseg63@gmail.com>
2022-09-15 21:37:27 +0400

    Project2 added

---
+++ Progect/Program.cs
@@ -0,0 +1,10 @@
+﻿
+int number = 12; // -999 999
+string name = "Hello, Oleg";
+float x = 5.1f; // -999.54
+
+bool flag = true; //false
+double b = 34.65;
+char type = 'd';
+
+var r = "hello";//Можно хранить все
